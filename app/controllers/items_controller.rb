f32f4bfcc09ce364b4bb3a40155c58af06a07040
Marie-Elise <membrq@gmail.com>
2016-09-15 23:08:50 -0500

    Checkpoint 7 WIP

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,19 +13,40 @@ class ItemsController < ApplicationController
     @user = User.find(params[:user_id])
     @item = @user.items.new(item_params)
     @item.user = current_user
-    #@new_item = Item.new
+    @new_item = Item.new
 
     if @item.save
       flash[:notice] = "Item saved successfully."
-      redirect_to [@user]
     else
       flash[:alert] = "Item failed to save."
-      redirect_to [@user]
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
   def show
     @item = Item.find(params[:user_id])
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @item = @user.items.find(item_params)
+
+    if @item.destroy
+      flash[:notice] = "Item was deleted."
+      #redirect_to [@user]
+    else
+      flash[:alert] = "Item couldn't be deleted. Try again!"
+      #redirect_to [@user]
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   private
